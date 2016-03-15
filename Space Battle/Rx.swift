1cda02b5f0300e658c4f9214a4f079e98d937fff
Marcin Wolicki <marcin.wolicki@gmail.com>
2016-03-15 01:30:13 +0000

    auto-reformat file

--- Space Battle/Rx.swift
+++ Space Battle/Rx.swift
@@ -8,38 +8,38 @@ import Foundation
 
 class Observable<T> {
     typealias A = T
-    private var subscribers:[(T->())] = []
-    func subscribe(fun:(T->())) -> Observable<T>  {
+    private var subscribers: [(T -> ())] = []
+    func subscribe(fun: (T -> ())) -> Observable<T> {
         subscribers.append(fun)
         return self
     }
 
-    func OnNext(el:T){
+    func OnNext(el: T) {
         for s in subscribers {
             s(el)
         }
     }
-
-
 }
 
 extension Observable {
-    func map<R>(mapper:(T->R)) -> Observable<R>{
-        let o=Observable<R>()
-        self.subscribe({x in o.OnNext(mapper (x))})
+    func map<R>(mapper: (T -> R)) -> Observable<R> {
+        let o = Observable<R>()
+        self.subscribe({ x in o.OnNext(mapper(x)) })
         return o
     }
 
-    func filter(predictor:(T->Bool)) -> Observable<T>{
-        let o=Observable<T>()
-        self.subscribe({x in if predictor(x) { o.OnNext(x)}})
+    func filter(predictor: (T -> Bool)) -> Observable<T> {
+        let o = Observable<T>()
+        self.subscribe({ x in if predictor(x) {
+            o.OnNext(x)
+        } })
         return o
     }
 
-    func merge(o1:Observable<T>, o2:Observable<T>) -> Observable<T>{
-        let o=Observable<T>()
-        o1.subscribe({x in o.OnNext(x)})
-        o2.subscribe({x in o.OnNext(x)})
+    func merge(o1: Observable<T>, o2: Observable<T>) -> Observable<T> {
+        let o = Observable<T>()
+        o1.subscribe({ x in o.OnNext(x) })
+        o2.subscribe({ x in o.OnNext(x) })
         return o
     }
 
